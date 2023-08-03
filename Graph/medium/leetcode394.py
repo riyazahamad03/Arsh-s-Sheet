b7a1d096adceb2ec7334474a9a4226fa15eaa4d2
RIYAZ AHAMED <riyazahamad397@gmail.com>
2023-08-03 20:13:45 +0530

    Add files via upload

---
+++ Graph/medium/leetcode394.py
@@ -0,0 +1,21 @@
+class solution:
+    def decodeString(self, s: str):
+        stack = []
+        for i in range(len(s)):
+            if s[i] != "]":
+                stack.append(s[i])
+            else:
+                substr = ""
+                while stack[-1] != "[":
+                    substr = stack.pop() + substr
+                stack.pop()
+
+                k = ""
+                while stack and stack[-1] in "0123456789":
+                    k = stack.pop() + k
+                stack.append(int(k) * substr)
+        return "".join(stack)
+
+
+x = solution()
+print(x.decodeString("3[a]2[bc]"))
