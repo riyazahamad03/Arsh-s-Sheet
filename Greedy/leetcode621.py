1614e1f4bea3be536062177e815dacf811830551
riyazahamad03 <riyazahameddo@gmail.com>
2023-08-22 21:16:30 +0530

    added leetcode solution

---
+++ Greedy/leetcode621.py
@@ -0,0 +1,26 @@
+import collections
+import heapq
+
+
+class Solution:
+    def leastInterval(self, tasks: list[str], n: int) -> int:
+        count = collections.Counter(tasks)
+        maxHeap = [-cnt for cnt in count.values()]
+        heapq.heapify(maxHeap)
+
+        time = 0
+        q = collections.deque()
+
+        while maxHeap or q:
+            time += 1
+            if maxHeap:
+                cnt = 1 + heapq.heappop(maxHeap)
+                if cnt:
+                    q.append([cnt, time + n])
+            if q and q[0][1] == time:
+                heapq.heappush(maxHeap, q.popleft()[0])
+        return time
+
+
+x = Solution()
+print(x.leastInterval(tasks=["A", "A", "A", "B", "B", "B"], n=2))
