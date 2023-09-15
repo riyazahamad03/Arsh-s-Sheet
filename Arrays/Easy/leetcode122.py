dffd4da52d9b427bf40642c5f5f2ad01737dbf71
riyazahamad03 <riyazahameddo@gmail.com>
2023-09-15 19:31:23 +0530

    added leetcode solution

--- Arrays/Easy/leetcode122.py
+++ Arrays/Easy/leetcode122.py
@@ -1,5 +1,5 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+    def maxProfit(self, prices: list[int]) -> int:
         r, l = 1, 0
         res = 0
         while r < len(prices):
